02b8d78aeb57c0ab41bdcf519c716df84cb8abdc
zys8119 <770959294@qq.com>
2020-10-19 15:39:13 +0800

    web

--- web/src/components/view/Home/Home.vue
+++ web/src/components/view/Home/Home.vue
@@ -23,19 +23,11 @@ export default {
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
-    h3 {
+    h1 {
         margin: 40px 0 0;
     }
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
-    a {
-        color: #42b983;
+    p{
+        margin-top: 50px;
     }
 }
 
--- web/src/components/view/Home/HomePage.vue
+++ web/src/components/view/Home/HomePage.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="HomePage">
-        <div class="topBanner"></div>
+        <div class="topBanner">
+            <img class="logo" :src="require('@/assets/web_logo1.png')" alt="">
+            <el-tabs class="el-tabs">
+                <el-tab-pane label="asdas"></el-tab-pane>
+                <el-tab-pane label="asdas"></el-tab-pane>
+                <el-tab-pane label="asdas"></el-tab-pane>
+            </el-tabs>
+        </div>
     </div>
 </template>
 
@@ -14,6 +21,31 @@ export default {
 .HomePage{
     .topBanner{
         background-color: @themeColor;
+        text-align: center;
+        padding-top: @unit15;
+        .logo{
+            width: 300px;
+            margin: 50px auto;
+            display: block;
+        }
+        /deep/ .el-tabs{
+            .el-tabs__item{
+                color: #ffffff;
+                &.is-active{
+                    background-color: #ffffff;
+                    color: @themeColor;
+                }
+            }
+            .el-tabs__active-bar{
+                background-color: #ffffff;
+            }
+            .el-tabs__header{
+                margin-bottom: 0;
+            }
+            .el-tabs__content{
+                display: none;
+            }
+        }
     }
 }
 </style>
